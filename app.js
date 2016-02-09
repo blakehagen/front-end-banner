87b5a72ab00ff74a81fb5286ba49a648735c3389
Blake Hagen <blakehagen@gmail.com>
2016-02-09 13:15:47 -0700

    read more button completed

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 $('.main-background img').hide();
+$('#less').hide();
 
 function fadeBackgroundImages() {
     $(".main-background img").first().appendTo('.main-background').fadeOut(2000);
-    $(".main-background img").first().fadeIn(2000);    
+    $(".main-background img").first().fadeIn(2000);
     setTimeout(fadeBackgroundImages, 3000);
 }
 fadeBackgroundImages();
+
+$(document).ready(function () {
+
+    $('.btn-container h4').click(function () {
+        $('.content').toggleClass('content-extend');
+        $('#less').toggle();
+        $('#more').toggle();
+    })
+
+
+});
